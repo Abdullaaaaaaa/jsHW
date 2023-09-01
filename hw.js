350c29a3adf4f5cb135a958f1999fbe66975998c
Abdullaaaaaaa <143504462+Abdullaaaaaaa@users.noreply.github.com>
2023-09-01 12:08:10 +0600

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,2 +1,19 @@
-console.log(55555);
-console.log(55555);console.log(55555);console.log(55555);
+//1- tapsyrma 
+let clothes = [shoes, coat, glove, jeans]; 
+clothes.push("suit"); 
+alert( clothes.pop() ); 
+ 
+//2- tapsyrma 
+function letterFinder(word, match) { 
+    for (let i = 0; i < word.length; i++); 
+} if (word[i] == match) { 
+    console.log('the', match, 'laptop', i); 
+} else { 
+    console.log('---No match the laptop', i); 
+} 
+ 
+letterFinder("lake", "l"); 
+ 
+//5- tapsyrma 
+const arr = [1,2,3,4,5]; 
+console.log(reverse([1, 2, 3, 4, 5]));
